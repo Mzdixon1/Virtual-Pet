f18203cb5ac347ff44dc2a6b53eb9ba6569d74c4
mzdixon1 <mzdixon1@yahoo.com>
2017-02-26 18:27:35 -0500

    First commit

--- Tiffanys VirtualPet/Program.cs
+++ Tiffanys VirtualPet/Program.cs
@@ -10,6 +10,63 @@ namespace Tiffanys_VirtualPet
     {
         static void Main(string[] args)
         {
+            string petName = string.Empty;
+
+            // user inputs 
+
+            int userInput;
+
+            VirtualPet Gizmo = new VirtualPet();
+
+            Console.WriteLine("What is your gremlins name?");
+            petName = Console.ReadLine();
+            Console.WriteLine("\n\n" + petName + " likes to eat Jack Frost Donuts, M&M's and drinks RedBull and must remain in the dark at all times");
+            do
+            {
+
+                Console.WriteLine("\n\nCurrently " + petName + " has:");
+                Gizmo.status();
+                userInput = int.Parse(Console.ReadLine());
+
+                //else if statements
+
+                if (userInput == 1)
+                {
+                    Gizmo.feed();
+                }
+                else if (userInput ==2)
+                {
+                    Gizmo.thirst();
+                }
+                
+            
+                else if (userInput == 3)
+            {
+                Gizmo.play();
+            }
+                else
+                {
+                    Console.WriteLine("Please enter valid input");
+                }
+            } while (true);
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
---
+++ Tiffanys VirtualPet/VirtualPet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiffanys_VirtualPet
+{
+    public class VirtualPet
+    {
+        //Called methods ints
+        private int petfood;
+        private int petthirst;
+        private int petplay;
+
+
+        public int petFood
+
+        {
+            get { return petfood; }
+            set { this.petfood = value; }
+        }
+
+        public int petThirst
+
+        {
+            get { return petthirst; }
+            set { this.petthirst = value; }
+        }
+        public int petPlay
+        {
+            get { return petplay; }
+            set { this.petplay = value; }
+        }
+
+
+
+        public VirtualPet()
+
+        {
+            petfood = 7;
+            petthirst = 7;
+            petplay = 7;
+        }
+        public void status()
+        {
+
+            Console.WriteLine("hunger: " + petFood + "\nthirst: " + petThirst + "\nboredom: " + petPlay);
+            Console.WriteLine("\nWhat would like to do?");
+            Console.WriteLine("to feed your gremlin type 1 ");
+            Console.WriteLine("to hydrate your gremline type 2 ");
+            Console.WriteLine("to play with your gremlin type 3, remember he does not like lights so only bring him out to play at night");
+
+        }
+        public void feed()
+        {
+            if (petfood == 0)
+            {
+                Console.WriteLine("not very hungry");
+            }
+            else
+            {
+                petfood = (petfood - 1);
+                
+            }
+              }
+        public void thirst()
+        {
+            if (petthirst == 0)
+            {
+                Console.WriteLine("not very thirsty");
+                
+            }
+            else
+            {
+                petthirst = (petthirst - 1);
+            }
+        }
+        public void play()
+        {
+            if (petplay == 0) 
+            {
+                Console.WriteLine("not in a playful mood");
+                
+            }
+            else
+            {
+                petplay = (petplay - 1);
+
+            }
+        }
+       
+    }
+
+}
